65c0e6632611c94bb36fd8a1fd1d2e044951d2bc
Mauro Morales <contact@mauromorales.com>
2012-10-11 09:55:48 +0300

    add ldap collect fields array
    add array to collect different fields from the ldap

--- lib/generators/devise_ldap_authenticatable/install_generator.rb
+++ lib/generators/devise_ldap_authenticatable/install_generator.rb
@@ -37,6 +37,7 @@ module DeviseLdapAuthenticatable
   # config.ldap_check_attributes = false
   # config.ldap_use_admin_to_bind = false
   # config.ldap_ad_group_check = false
+  # config.ldap_collect_fields = []
   
       eof
       if options.advanced?  
